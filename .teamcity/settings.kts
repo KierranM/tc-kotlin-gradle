c0f5f06f56efad03ef3f99f33ea5967938cf818c
Kierran McPherson <kierranm@gmail.com>
2019-08-27 10:35:29 +1200

    Install android sdk in build

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3,6 +3,7 @@ import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.PullRequests
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.commitStatusPublisher
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.pullRequests
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
 
 version = "2019.1"
@@ -19,6 +20,24 @@ object AndroidGradleBuild : BuildType({
         cleanCheckout = true
     }
     steps {
+        script {
+            name = "Download Android SDK"
+            scriptContent = """
+                #!/bin/bash -e
+                
+                wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
+                
+                # unpack archive
+                unzip sdk-tools-linux-4333796.zip
+                
+                rm sdk-tools-linux-4333796.zip
+                
+                mkdir android-sdk
+                mv tools android-sdk/tools
+                
+                echo "sdk.dir=$(pwd)/android-sdk/" >> local.properties
+            """.trimIndent()
+        }
         gradle {}
     }
 
